581de970aa30f6cee03fdb56915a2fb054d45dbd
Kyle Amyx <kyle.amyx@duethealth.com>
2019-01-10 11:09:37 -0500

    first commit, step 8

---
+++ src/com/google/sample/cast/refplayer/CastOptionsProvider.java
@@ -0,0 +1,26 @@
+package com.google.sample.cast.refplayer;
+
+import android.content.Context;
+
+import com.google.android.gms.cast.framework.CastOptions;
+import com.google.android.gms.cast.framework.OptionsProvider;
+import com.google.android.gms.cast.framework.SessionProvider;
+
+import java.util.List;
+
+//Added to Manifest
+
+public class CastOptionsProvider implements OptionsProvider {
+
+    @Override
+    public CastOptions getCastOptions(Context context) {
+        return new CastOptions.Builder()
+                .setReceiverApplicationId(context.getString(R.string.app_id))
+                .build();
+    }
+
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return null;
+    }
+}
